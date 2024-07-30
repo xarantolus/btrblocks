44146a8f6cbd7330e3452cb0f516f922584864a6
xarantolus <xarantolus@protonmail.com>
2024-07-30 12:19:34 +0000

    Upgrade google benchmark
    This prevents a compile error due to an unused variable warning

--- cmake/benchmark.cmake
+++ cmake/benchmark.cmake
@@ -9,13 +9,13 @@ ExternalProject_Add(
     benchmark_src
     PREFIX "vendor/gbenchmark"
     GIT_REPOSITORY "https://github.com/google/benchmark.git"
-    GIT_TAG 336bb8db986cc52cdf0cefa0a7378b9567d1afee
+    GIT_TAG 378fe693a1ef51500db21b11ff05a8018c5f0e55
     TIMEOUT 10
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/vendor/gbenchmark
         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
         -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} -Wunused-but-set-variable
         -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     UPDATE_COMMAND ""
 )
