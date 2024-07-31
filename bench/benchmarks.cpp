d3ce34c848e1b713a58051628116b4257668909a
xarantolus <xarantolus@protonmail.com>
2024-07-31 08:18:36 +0000

    Benchmarks: specify if SVE is used

--- bench/benchmarks.cpp
+++ bench/benchmarks.cpp
@@ -2,6 +2,7 @@
 // BtrBlocks
 // ---------------------------------------------------------------------------
 #include "benchmark/benchmark.h"
+#include "common/SIMD.hpp"
 #include "scheme/SchemePool.hpp"
 #include "bench-cases/regression_benchmark.cpp"
 // ---------------------------------------------------------------------------
@@ -9,7 +10,11 @@ using namespace btrblocks;
 // ---------------------------------------------------------------------------
 int main(int argc, char** argv) {
 #ifdef BTR_USE_SIMD
-  std::cout << "\033[0;35mSIMD ENABLED\033[0m" << std::endl;
+  if (SVE_ENABLED) {
+    std::cout << "\033[0;35mSIMD+SVE ENABLED\033[0m" << std::endl;
+  } else {
+    std::cout << "\033[0;35mSIMD ENABLED\033[0m" << std::endl;
+  }
 #else
   std::cout << "\033[0;31mSIMD DISABLED\033[0m" << std::endl;
 #endif
