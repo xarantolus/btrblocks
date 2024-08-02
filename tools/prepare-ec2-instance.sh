cc33dd7ac2e7f97e0238b51564b8f108ddb23816
xarantolus <xarantolus@protonmail.com>
2024-08-02 13:09:12 +0000

    Preparation script

--- tools/prepare-ec2-instance.sh
+++ tools/prepare-ec2-instance.sh
@@ -8,27 +8,4 @@ set -eu
 
 # Dependencies
 sudo yum -y groupinstall "Development Tools"
-sudo yum -y install openssl-devel libcurl-devel bzip2-devel postgresql-devel gcc-c++ tmux git htop tree perf boost-devel
-
-pip3 install pandas pyarrow pyspark pyyaml
-
-# Add symlinks to newer gcc versions
-mkdir $HOME/bin
-pushd $HOME/bin
-ln -sf /usr/bin/gcc10-g++ g++
-ln -sf /usr/bin/gcc10-c++ c++
-ln -sf /usr/bin/gcc10-gcc gcc
-ln -sf /usr/bin/gcc10-cc cc
-ln -sf /usr/bin/gcc10-cpp cpp
-popd # $HOME/bin
-
-version='3.23.1'
-threads=32
-
-rm -rf "cmake-${version}"
-wget "https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}.tar.gz"
-tar xf "cmake-${version}.tar.gz"
-cd "cmake-${version}"
-./configure --parallel=${threads}
-make -j ${threads}
-sudo make install
+sudo yum -y install openssl-devel libcurl-devel bzip2-devel postgresql-devel gcc-c++ tmux git htop tree perf boost-devel cmake tbb tbb-devel
