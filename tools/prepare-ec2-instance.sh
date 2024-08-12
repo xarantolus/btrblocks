b7f10b40badb0204d320798be10351de11687e56
xarantolus <xarantolus@protonmail.com>
2024-08-12 12:25:39 +0000

    Adjust preparation script

--- tools/prepare-ec2-instance.sh
+++ tools/prepare-ec2-instance.sh
@@ -8,4 +8,19 @@ set -eu
 
 # Dependencies
 sudo yum -y groupinstall "Development Tools"
-sudo yum -y install openssl-devel libcurl-devel bzip2-devel postgresql-devel gcc-c++ tmux git htop tree perf boost-devel cmake tbb tbb-devel zlib-devel
+sudo yum -y install openssl-devel libcurl-devel bzip2-devel postgresql-devel tmux git htop tree perf boost-devel cmake tbb tbb-devel zlib-devel
+
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+./llvm.sh 18 all
+ln -s /usr/bin/clang-18 /usr/bin/clang
+ln -s /usr/bin/clang++-18 /usr/bin/clang++
+ln -s /usr/bin/clangd-18 /usr/bin/clangd
+ln -s /usr/bin/clang-format-18 /usr/bin/clang-format
+ln -s /usr/bin/clang-tidy-18 /usr/bin/clang-tidy
+ln -s /usr/bin/ld.lld-18 /usr/bin/ld.lld
+ln -s /usr/bin/lld-18 /usr/bin/lld
+ln -s /usr/bin/lldb-18 /usr/bin/lldb
+update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
+update-alternatives --install /usr/bin/ld ld /usr/bin/ld.lld 100
