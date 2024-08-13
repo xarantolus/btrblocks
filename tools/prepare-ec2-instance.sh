b4747859914aa6a8a9b541da4103000c868b0d85
xarantolus <xarantolus@protonmail.com>
2024-08-13 13:33:17 +0000

    Use clang only on Ubuntu

--- tools/prepare-ec2-instance.sh
+++ tools/prepare-ec2-instance.sh
@@ -10,23 +10,23 @@ if command -v apt > /dev/null; then
     # Use apt for dependencies
     sudo apt update
     sudo apt -y install build-essential libssl-dev libcurl4-openssl-dev libbz2-dev libpq-dev tmux git htop tree linux-tools-common libboost-all-dev cmake libtbb-dev zlib1g-dev lsb-release wget gnupg
+
+    wget https://apt.llvm.org/llvm.sh
+    chmod +x llvm.sh
+    sudo ./llvm.sh 18 all
+    sudo ln -s /usr/bin/clang-18 /usr/bin/clang
+    sudo ln -s /usr/bin/clang++-18 /usr/bin/clang++
+    sudo ln -s /usr/bin/clangd-18 /usr/bin/clangd
+    sudo ln -s /usr/bin/clang-format-18 /usr/bin/clang-format
+    sudo ln -s /usr/bin/clang-tidy-18 /usr/bin/clang-tidy
+    sudo ln -s /usr/bin/ld.lld-18 /usr/bin/ld.lld
+    sudo ln -s /usr/bin/lld-18 /usr/bin/lld
+    sudo ln -s /usr/bin/lldb-18 /usr/bin/lldb
+    sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+    sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
+    sudo update-alternatives --install /usr/bin/ld ld /usr/bin/ld.lld 100
 else
     # Use yum for dependencies
     sudo yum -y groupinstall "Development Tools"
     sudo yum -y install openssl-devel libcurl-devel bzip2-devel postgresql-devel tmux git htop tree perf boost-devel cmake tbb tbb-devel zlib-devel wget gnupg
 fi
-
-wget https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-sudo ./llvm.sh 18 all
-sudo ln -s /usr/bin/clang-18 /usr/bin/clang
-sudo ln -s /usr/bin/clang++-18 /usr/bin/clang++
-sudo ln -s /usr/bin/clangd-18 /usr/bin/clangd
-sudo ln -s /usr/bin/clang-format-18 /usr/bin/clang-format
-sudo ln -s /usr/bin/clang-tidy-18 /usr/bin/clang-tidy
-sudo ln -s /usr/bin/ld.lld-18 /usr/bin/ld.lld
-sudo ln -s /usr/bin/lld-18 /usr/bin/lld
-sudo ln -s /usr/bin/lldb-18 /usr/bin/lldb
-sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
-sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
-sudo update-alternatives --install /usr/bin/ld ld /usr/bin/ld.lld 100
